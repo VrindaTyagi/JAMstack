f8dd5669477428b4a6494563db818598afe759ec
Vrinda Tyagi <vrindatyagi09@gmail.com>
2021-03-19 11:29:10 +0530

    build

---
+++ build/precache-manifest.53fa505cd96a97f51b4cdb8346939bac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec8077f41da24a7fa7f9001d9829c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5afe86f9cc2b750e1d6f",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1c07192e4ac0124a4686",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5afe86f9cc2b750e1d6f",
+    "url": "/static/js/2.6e3274c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e3274c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c07192e4ac0124a4686",
+    "url": "/static/js/main.5ad40ad8.chunk.js"
+  },
+  {
+    "revision": "0c0e1140668c56ed1770",
+    "url": "/static/js/runtime-main.e0700b62.js"
+  }
+]);
